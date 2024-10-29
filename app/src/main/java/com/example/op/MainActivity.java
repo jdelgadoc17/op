f57d76909fa6977d93e006f540c003ad04509b06
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-10-30 00:58:43 +0100

    ++

--- app/src/main/java/com/example/op/MainActivity.java
+++ app/src/main/java/com/example/op/MainActivity.java
@@ -22,6 +22,26 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.op.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationBarView;
 
+
+import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.op.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
@@ -38,18 +58,48 @@ public class MainActivity extends AppCompatActivity {
         // Inicializa PersonajeViewModel
         personajeViewModel = new ViewModelProvider(this).get(PersonajeViewModel.class);
 
-        // Configuración de la Toolbar y el BottomNavigationView (asegurarse de que esté en uso el NavController correcto)
+        // Configuración de la Toolbar
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getString(R.string.app_name));
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        toolbar.setBackground(new ColorDrawable(getResources().getColor(R.color.blueOnePiece)));
+
+        // Configuración del menú inferior con vistaLista
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.vistaLista);
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            NavigationUI.setupWithNavController(binding.menuInferior, navController);
+
+            // Configuración de iconos y tamaño del menú
+            binding.menuInferior.setItemIconTintList(null);
+            binding.menuInferior.setItemIconSize(150);
+
+            // Listener para el menú inferior
+            binding.menuInferior.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    if (item.getItemId() == R.id.fragmentoLista) {
+                        if (navController.getCurrentDestination() != null
+                                && navController.getCurrentDestination().getId() == R.id.fragmentoLista) {
+                            return true;
+                        }
+                        navController.navigate(R.id.fragmentoLista);
+                    } else if (item.getItemId() == R.id.fragmentoFavoritos) {
+                        navController.navigate(R.id.fragmentoFavoritos);
+                    } else if (item.getItemId() == R.id.fragmentoRandom) {
+                        navController.navigate(R.id.fragmentoRandom);
+                    }
+                    return true;
+                }
+            });
+        }
 
         // Observador del personaje seleccionado
         personajeViewModel.getPersonajeSeleccionado().observe(this, personaje -> {
-            // En modo horizontal, muestra y actualiza vistaDetalle con el personaje seleccionado
+            // Si estamos en modo horizontal, asegura que vistaDetalle muestre el personaje
             if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                Fragment vistaDetalle = getSupportFragmentManager().findFragmentById(R.id.vistaDetalle);
-                if (vistaDetalle != null && vistaDetalle.getView() != null) {
-                    vistaDetalle.getView().setVisibility(View.VISIBLE);
-                    // Aquí se asegura de que FragmentoDetalle tenga el personaje actualizado
-                    actualizarVistaDetalle(personaje);
-                }
+                actualizarVistaDetalle(personaje);
             }
         });
 
@@ -61,6 +111,14 @@ public class MainActivity extends AppCompatActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         ajustarVisibilidadFragmentos(newConfig.orientation);
+
+        // Si cambiamos a modo horizontal, actualiza vistaDetalle con el personaje seleccionado
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Personaje personajeSeleccionado = personajeViewModel.getPersonajeSeleccionado().getValue();
+            if (personajeSeleccionado != null) {
+                actualizarVistaDetalle(personajeSeleccionado);
+            }
+        }
     }
 
     private void ajustarVisibilidadFragmentos(int orientation) {
@@ -68,15 +126,15 @@ public class MainActivity extends AppCompatActivity {
         Fragment vistaDetalle = getSupportFragmentManager().findFragmentById(R.id.vistaDetalle);
 
         if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // En modo horizontal, ambos fragmentos son visibles
             if (vistaLista != null && vistaLista.getView() != null) {
                 vistaLista.getView().setVisibility(View.VISIBLE);
             }
             if (vistaDetalle != null && vistaDetalle.getView() != null) {
                 vistaDetalle.getView().setVisibility(View.VISIBLE);
-                // Forzar la actualización del personaje seleccionado en vistaDetalle
-                personajeViewModel.getPersonajeSeleccionado().getValue();
             }
         } else {
+            // En modo vertical, solo vistaLista es visible al inicio, oculta vistaDetalle
             if (vistaLista != null && vistaLista.getView() != null) {
                 vistaLista.getView().setVisibility(View.VISIBLE);
             }
@@ -98,10 +156,4 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
-
-
-
-
-
 }
